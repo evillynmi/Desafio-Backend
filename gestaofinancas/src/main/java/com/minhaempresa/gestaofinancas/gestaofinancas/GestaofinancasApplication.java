ac5ecf21d9094ff12275896e0e9a4ccd830adcbf
Milena <evillynmilena@usp.br>
2024-03-25 20:20:25 -0300

    Criando o projeto

---
+++ gestaofinancas/src/main/java/com/minhaempresa/gestaofinancas/gestaofinancas/GestaofinancasApplication.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.gestaofinancas.gestaofinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofinancasApplication.class, args);
+	}
+
+}
---
+++ gestaofinancas/src/test/java/com/minhaempresa/gestaofinancas/gestaofinancas/GestaofinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaempresa.gestaofinancas.gestaofinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaofinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
