3bb6b7d5f13e3a49725818a74d65039704549016
Milena <evillynmilena@usp.br>
2024-03-26 22:15:31 -0300

    Update Conta.java

--- gestaofinancas/src/main/java/com/minhaempresa/gestaofinancas/gestaofinancas/domain/model/Conta.java
+++ gestaofinancas/src/main/java/com/minhaempresa/gestaofinancas/gestaofinancas/domain/model/Conta.java
@@ -3,12 +3,10 @@ package com.minhaempresa.gestaofinancas.gestaofinancas.domain.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "contas")
 public class Conta {
 
     @Id
